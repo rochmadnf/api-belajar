77e7de1c1cba87764e14d3a13c12487f536e2b0c
rochmadnf <rochmadnf@gmail.com>
2022-10-29 14:04:42 +0800

    using resource when get data user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\{LoginController, LogoutController, RegisterController};
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +10,6 @@ Route::post('/register', RegisterController::class);
 Route::post('/login', LoginController::class);
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/user', fn (Request $request) => $request->user());
+    Route::get('/user', fn (Request $request) => new UserResource($request->user()));
     Route::post('/logout', LogoutController::class);
 });
